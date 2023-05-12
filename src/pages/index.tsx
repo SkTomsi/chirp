ac4682c1e49d4941e9713836e9593e89c1a67597
TomcyT <tomcy48@gmail.com>
2023-05-12 15:26:03 +0530

    loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,65 +7,73 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
 
 dayjs.extend(relativeTime);
+const CreatePostWizard = () => {
+  const { user } = useUser();
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-4">
+      <Image
+        src={user.profileImageUrl}
+        alt="profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        type="text"
+        placeholder="Type some emojis"
+        className="p grow bg-transparent outline-none"
+      />
+    </div>
+  );
+};
+
+type postWithUser = RouterOutputs["postsRouter"]["getAll"][number];
+const PostView = (props: postWithUser) => {
+  const { post, author } = props;
+  return (
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.profileImageUrl}
+        alt={`${author.username}'s profile image}`}
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex gap-1  text-slate-300">
+          {`@${author.username}`}
+          <span>·</span>
+          <span>{dayjs(post.createdAt).fromNow()}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </div>
+  );
+};
+
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.postsRouter.getAll.useQuery();
+
+  if (postsLoading) return <LoadingPage />;
+  if (!data) throw new Error("Something is wrong...");
+  return (
+    <div className="flex flex-col">
+      {[...data, ...data]?.map((fullpost) => (
+        <PostView {...fullpost} key={fullpost.post.id} />
+      ))}
+    </div>
+  );
+};
 
 const Home: NextPage = () => {
-  const user = useUser();
-
-  console.log(user);
-
-  const CreatePostWizard = () => {
-    const { user } = useUser();
-    if (!user) return null;
-
-    return (
-      <div className="flex w-full gap-4">
-        <Image
-          src={user.profileImageUrl}
-          alt="profile image"
-          className="h-14 w-14 rounded-full"
-          width={56}
-          height={56}
-        />
-        <input
-          type="text"
-          placeholder="Type some emojis"
-          className="p grow bg-transparent outline-none"
-        />
-      </div>
-    );
-  };
-
-  // we want an element from this array type,
-  type postWithUser = RouterOutputs["postsRouter"]["getAll"][number];
-  const PostView = (props: postWithUser) => {
-    const { post, author } = props;
-    return (
-      <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-        <Image
-          src={author.profileImageUrl}
-          alt={`${author.username}'s profile image}`}
-          className="h-14 w-14 rounded-full"
-          width={56}
-          height={56}
-        />
-        <div className="flex flex-col">
-          <div className="flex gap-1  text-slate-300">
-            {`@${author.username}`}
-            <span>·</span>
-            <span>{dayjs(post.createdAt).fromNow()}</span>
-          </div>
-          <span>{post.content}</span>
-        </div>
-      </div>
-    );
-  };
-
-  const { data, isLoading } = api.postsRouter.getAll.useQuery();
-
-  if (isLoading) return <div>loading....</div>;
-  if (!data) return <div>Something is wrong...</div>;
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
+  api.postsRouter.getAll.useQuery();
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -77,18 +85,14 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
         <div className="w-full border-x border-slate-500 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
-            {!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 <SignInButton />
               </div>
             )}
-            {user.isSignedIn && <CreatePostWizard />}
+            {isSignedIn && <CreatePostWizard />}
           </div>
-          <div className="flex flex-col">
-            {[...data, ...data]?.map((fullpost) => (
-              <PostView {...fullpost} key={fullpost.post.id} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
