61b94c7da928228c1fb6ac38d1d685e666da9565
TomcyT <tomcy48@gmail.com>
2023-05-30 07:41:10 +0100

    add posting and loading states + toast on error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import Head from "next/head";
 import { useUser, SignInButton } from "@clerk/nextjs";
 
 import { type RouterOutputs, api } from "~/utils/api";
-
+import { toast } from "react-hot-toast";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 
 dayjs.extend(relativeTime);
@@ -19,6 +19,15 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.postsRouter.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+        return;
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
     },
   });
 
@@ -40,8 +49,24 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter")
+            if (input !== "") {
+              mutate({ content: input });
+            }
+        }}
       />
-      <button onClick={() => mutate({ content: input })}>Tweet</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>
+          Tweet
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
